b9d30b294b08e1285e51eaaf7b1c6bb14db0ba22
lcaprabu <41501260+lcaprabu@users.noreply.github.com>
2022-01-12 09:11:04 +0000

    "List group with div"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,22 @@ import './App.css';
 function App() {
   return (
   
-    <div style={{padding:'10px',display:'flex',height:'100%',width:'100%',backgroundColor:'#DEEAE8'}}>
-     <div style={{margin:'2px',height:'100%',width:'100px'}}>
+    <div style={{padding:'10px',display:'flex',height:'100%',width:'300%',backgroundColor:'#DEEAE8'}}>
+     <div style={{margin:'2px',height:'100%',width:'300px'}}>
      <ul className="App">
-        <li>Adele</li>
-        <li>Agnes</li>
-        <li>Billy</li>
-        <li>Bob</li>
-        <li>Calvin</li>
-        <li>Christina</li>
-        <li>Cindy</li>
+        <hl><h4>FP Inventory</h4></hl>
+        <li>Adele  (20)</li>
+        <li>Agnes  (60)</li>
+        <li>Billy  (10)</li>
+        <li>Bob  (8)</li>
+        <li>Calvin  (20)</li>
+        <li>Christina  (3)</li>
+        <li>Cindy  (11)</li>
       </ul>
      </div>
-     <div style={{margin:'2px',height:'100%',width:'100px'}}>
+     <div style={{marginLeft:'60px',margin:'2px',height:'100%',width:'300px'}}>
      <ul className="App">
+     <hl><h4>CGMD Inventory</h4></hl>
         <li>Adele</li>
         <li>Agnes</li>
         <li>Billy</li>
